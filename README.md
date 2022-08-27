67e0047861d7a42b11c0b0fe18e94313eded795f
passportpowell <otisofro@gmail.com>
2022-08-27 19:15:02 +0000

    restarted project as made too many mistakes and was unsure where i went wrong

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # project-2
 
-2 Player Quiz game
-or
 1 player Multiple choice Game
 
 
 ## options
-- maths
-- geography capital cities*
-- riddles
 
-## split screen
-- Take in turns
-- scores under each player
-- progress bar next to each player
-- 
+- geography capital cities*
+- option to remove an answer up 1 times per quesion (max of 3 uses overall)
+- scores 
+- progress bar 
+
+
+## bugs
+javascript for choices wasn't allowing me to check if answer was correct. Had to change "choicea" to "choice1" for it to work.
+
+Math function wasn't working correctly. corrected by using .length on 'let noOfQuestions []' as each time a question if presented the total number would decrease. helps in stopping the same question appearing.
+
+Questions inside questions array was not showing up and chanchging but instead showed as "undefined". fixed by changing "class=question" to "id=question" in html.
+
+After solecting correct answer nothing would happen and next question would not load. fixed by checking my comments to see i had commented out code instead of commenting next to it. fixed the error
+
+questions moved on to fast before showing whether or not the chosen answer was correct via colors. fixed by finiding out how to set a time limit before moving onto next question
+
+all answers are showing as incoorect. fixed by checking my syntax in my ternary attempt. i had it compare to "acceptedA" instead of answer in the questions array
+
+trying to apply a class to the right and wrong answer respectivly wasn't working. fixed by changin "options" class into an options id.
---
+++ assets/js/script.js
@@ -0,0 +1,173 @@
+// use arrow syntax to make functions concise. e.g. () => {}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+})
+
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("options"));
+console.log(choices)
+
+let currentQ = {}
+let acceptedA = false // Stops players from starting the game before everything loads
+let currentScore = 0
+let questionNo = 0
+let noOfQuestions = []
+
+let questions = [
+    {
+        question: "What is the capital city of England?",
+        choice1: "Birmingham",
+        choice2: "London",
+        choice3: "Northfield",
+        choice4: "Elizabeth",
+        answer: 2,
+    },
+
+    {
+        question: "What is the capital city of Northern Ireland?",
+        choice1: "Wales",
+        choice2: "Belfast",
+        choice3: "Southern Ireland",
+        choice4: "Elizabeth",
+        answer: 2,
+    },
+    {
+        question: "What is the capital city of Scotland?",
+        choice1: "Edinburgh ",
+        choice2: "London",
+        choice3: "Mayfield",
+        choice4: "Boston",
+        answer: 1,
+    },
+    {
+        question: "What is the capital city of Wales?",
+        choice1: "Ireland",
+        choice2: "London",
+        choice3: "Cardiff",
+        choice4: "Elizabeth",
+        answer: 3,
+    },
+    {
+        question: "What is the capital city of Spain?",
+        choice1: "Birmingham",
+        choice2: "London",
+        choice3: "Northfield",
+        choice4: "Madrid",
+        answer: 4,
+    },
+    {
+        question: "What is the capital city of Italy?",
+        choice1: "Rome",
+        choice2: "London",
+        choice3: "Northfield",
+        choice4: "Elizabeth",
+        answer: 1,
+    },
+    {
+        question: "What is the capital city of Germany?",
+        choice1: "Berlin",
+        choice2: "London",
+        choice3: "Northfield",
+        choice4: "Elizabeth",
+        answer: 1,
+    },
+    {
+        question: "What is the capital city of France?",
+        choice1: "Birmingham",
+        choice2: "London",
+        choice3: "Paris",
+        choice4: "Elizabeth",
+        answer: 3,
+    },
+    {
+        question: "What is the capital city of Greece?",
+        choice1: "Birmingham",
+        choice2: "Athens",
+        choice3: "Northfield",
+        choice4: "Elizabeth",
+        answer: 2,
+    },
+    {
+        question: "What is the capital city of North Macedonia?",
+        choice1: "Birmingham",
+        choice2: "London",
+        choice3: "Skopje",
+        choice4: "Elizabeth",
+        answer: 3,
+    },
+]
+
+const correctIncrement = 10
+const maxQuestions = 10
+
+startGame = () => {
+    questionNo = 0
+    currentScore = 0
+    noOfQuestions = [...questions]    //  use ... (spread operator) to make a full copy of all questions
+    console.log(noOfQuestions)
+    nextQuestion()
+}
+
+nextQuestion = () => {
+    questionNo++
+    const questionsIndex = Math.floor(Math.random() * noOfQuestions.length)
+    currentQ = noOfQuestions[questionsIndex]
+    question.innerText = currentQ.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"]
+        choice.innerText = currentQ["choice" + number]
+    })
+
+    noOfQuestions.splice(questionsIndex, 1) //  This should remove the question from the game so it doesnt repeat
+    acceptedA = true    // This allows players to answer the questions
+
+    if (noOfQuestions.length === 0 || questionNo >= maxQuestions) {
+        alert("Game Over!" +
+            " Thanks for playing :-)")
+    }
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        console.log(e.target)// This is to check that the button clicks matches what's excpected via console
+        if (!acceptedA) return
+        acceptedA = false;
+        const chosenButton = e.target
+        const chosenChoice = chosenButton.dataset["number"] // to fix - something is going wrong here
+
+        //try some ternary
+        const applyClass = chosenChoice == currentQ.answer ? "Right" : "Wrong"
+        console.log(applyClass) //Checking if it worked
+
+        chosenButton.classList.add(applyClass)
+
+
+
+        setTimeout(() => {
+            // chosenButton.classList.remove(applyClass)
+            nextQuestion()
+        }, 1000)
+
+
+
+    })
+})
+
+startGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
